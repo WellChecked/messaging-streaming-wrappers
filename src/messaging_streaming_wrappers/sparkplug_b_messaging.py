68040d5b0454d9e3d78fc45cbcbd9fa094c3690b
Hank Haines <hankhaines@seqtechllc.com>
2025-02-06 14:58:09 -0600

    Removed metric.value logging

--- src/messaging_streaming_wrappers/sparkplug_b_messaging.py
+++ src/messaging_streaming_wrappers/sparkplug_b_messaging.py
@@ -78,22 +78,22 @@ class SparkplugBMessageManager(MqttMessageManager):
         return spb_payload
 
     def on_ncmd_message(self, topic: str, message: Any, params: dict = None):
-        print(f"Received message on topic {topic}: {message} --- {params}")
+        print(f"Received NCMD message on topic {topic}: {message} --- {params}")
         rebirth = False
         payload = self.parse_sparkplug_b_payload(message)
         for metric in payload.metrics:
-            log.debug(f"Node control [{metric.name}] for [{self.edge_node.group}/{self.edge_node.node}]: {metric.value}")
+            log.debug(f"Node control [{metric.name}] for [{self.edge_node.group}/{self.edge_node.node}]")
             if not rebirth:
                 self._publish_node_birth(self._mqtt_client)
                 self._publish_device_births(self._mqtt_client)
             rebirth = True
 
     def on_dcmd_message(self, topic: str, message: Any, params: dict = None):
-        print(f"Received message on topic {topic}: {message} --- {params}")
+        print(f"Received DCMD message on topic {topic}: {message} --- {params}")
         rebirth = False
         payload = self.parse_sparkplug_b_payload(message)
         for metric in payload.metrics:
-            log.debug(f"Data control [{metric.name}] for [{self.edge_node.group}/{self.edge_node.node}]: {metric.value}")
+            log.debug(f"Data control [{metric.name}] for [{self.edge_node.group}/{self.edge_node.node}]")
             if not rebirth:
                 self._publish_node_birth(self._mqtt_client)
                 self._publish_device_births(self._mqtt_client)
